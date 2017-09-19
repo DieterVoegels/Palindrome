dd81f90dfaaf6f4dcae3c1c4ab75de86117d0404
U-BSD\333369 <333369@SHS-10D4C15EA.beaverton.k12.or.us>
2017-09-19 11:11:18 -0700

    First

---
+++ Palindrome.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string.h>
+#include <ctype.h>
+/*Program: return if an input is a palinrome or not
+  Author:Dieter Voegels
+  date: 9/19
+ */
+
+
+using namespace std;
+//main function
+int main(){
+  //initialize
+  char in[80];
+  char newstr[80];
+  char out[80];
+  char check;
+  int count = 0;
+  memset (in, '\0', 80);
+  //get input
+  cin.get(in, 80);
+  //get rid of anything that is not a letter
+  for (int i = 0; i < 80; i++){
+    check = in[i];
+    if (isalpha(check)){
+      newstr[count] = check;
+      count++;
+    }
+  }
+  //insert null character
+  newstr[count] = '\0';
+  out[count] = '\0';
+  count--;
+  //check if its a palindrome
+  for (int i = 0; i < 80; i++){
+    if (count >= 0){
+      out[i] = newstr[count];
+      count--;
+    }
+  }
+  //return answer
+  if(strcmp(newstr,out) == 0){
+    cout << "Palindrome" << endl;
+  }
+  else{
+    cout << "Not a palindrome" << endl;
+  }
+
+  return 0;
+}
